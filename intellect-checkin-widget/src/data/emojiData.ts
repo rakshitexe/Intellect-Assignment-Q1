5cc88f3eccd675245d799ee7dd21cd694fc2a63a
rakshitexe <rakshit.cse@gmail.com>
2025-07-17 00:05:18 +0530

    icon pack updated

--- intellect-checkin-widget/src/data/emojiData.ts
+++ intellect-checkin-widget/src/data/emojiData.ts
@@ -1,5 +1,8 @@
 import AlrightIcon from '../assets/icons/emoji/alright.png'
 import BadIcon from '../assets/icons/emoji/bad.png'
+import FantasticIcon from '../assets/icons/emoji/fantastic.png'
+import TerribleIcon from '../assets/icons/emoji/terrible.png'
+import PrettyGoodIcon from '../assets/icons/emoji/prettygood.png'
 
 export interface EmojiOption {
   id: number
@@ -15,10 +18,10 @@ export interface EmojiCheckinConfig {
 export const emojiCheckinData: EmojiCheckinConfig = {
   question: "Hello! How are you feeling today?",
   options: [
-    { id: 1, iconPath: AlrightIcon, label: "Terrible" },
+    { id: 1, iconPath: TerribleIcon, label: "Terrible" },
     { id: 2, iconPath: BadIcon, label: "Bad" },
-    { id: 3, iconPath: BadIcon, label: "Alright" },
-    { id: 4, iconPath: AlrightIcon, label: "Pretty Good" },
-    { id: 5, iconPath: BadIcon, label: "Fantastic" },
+    { id: 3, iconPath: AlrightIcon, label: "Alright" },
+    { id: 4, iconPath: PrettyGoodIcon, label: "Pretty Good" },
+    { id: 5, iconPath: FantasticIcon, label: "Fantastic" },
   ]
 }
